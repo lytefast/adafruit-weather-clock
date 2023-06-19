0607c9f0eff9c223ce91c2a449cedd231325b705
Sam S <lytefast@gmail.com>
2023-06-19 15:10:05 -0700

    fix logging: logging new library

--- aio_handler.py
+++ aio_handler.py
@@ -28,9 +28,9 @@ from adafruit_portalbase.network import NetworkBase
 # l.level = logging.ERROR
 # l.error("test")
 
-from adafruit_logging import LoggingHandler
+from adafruit_logging import Handler, LogRecord
 
-class AIOHandler(LoggingHandler):
+class AIOHandler(Handler):
 
     def __init__(self, name, portal_device, log_level=logging.INFO):
         """Create an instance."""
@@ -38,21 +38,23 @@ class AIOHandler(LoggingHandler):
         if not issubclass(type(portal_device), (PortalBase, NetworkBase)):
             raise TypeError("portal_device must be a NetworkBase or subclass of PortalBase")
         self._portal_device = portal_device
+        self.setLevel(log_level)
         self.send_log_level = log_level
 
 
-    def emit(self, log_level: int, message: str):
+    # def emit(self, log_level: int, message: str):
+    def emit(self, log: LogRecord):
         """Generate the message and write it to the AIO Feed.
 
         :param level: The level at which to log
         :param msg: The core message
 
         """
-        if log_level < self.send_log_level:
+        if log.levelno < self.send_log_level:
             return
         try:
             self._portal_device.push_to_io(
-                self._log_feed_name, self.format(log_level, message)
+                self._log_feed_name, self.format(log.levelno, log.msg)
             )
         except:
             # something went wrong but don't kill the caller
